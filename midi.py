26a0c11241faeee390ffc92ec9facaa09f13bf47
Aman Choudhary <amanch@ic-tsloan-m057.local>
2023-10-31 20:45:40 -0400

    removed print statements, cleaned up code

--- midi.py
+++ midi.py
@@ -1,12 +1,7 @@
 import mido
 import time
-# import rtmidi
 
-# midi_in = rtmidi.MidiIn()
-# input_ports = midi_in.get_ports()
-# print(input_ports)
-
-print("welcome to the game")
+print("Welcome!")
 
 existing_sequence = []
 midi_file = mido.MidiFile('test.mid')
@@ -14,12 +9,12 @@ midi_file = mido.MidiFile('test.mid')
 ticks_per_beat = midi_file.ticks_per_beat
 current_tempo = 500000  
 
-
 count = 0
 start_time = -1
 
 time_tracker = 0
 real_time = []
+
 for i, track in enumerate(midi_file.tracks):
     for msg in track:
         if type(msg) is not mido.MetaMessage:
@@ -28,17 +23,9 @@ for i, track in enumerate(midi_file.tracks):
                 real_time.append(time_tracker)
                 existing_sequence.append(msg)
 
-print(existing_sequence)
-print(real_time)
-
 # Function to compare two MIDI sequences
 def compare_midi_sequences(midi_file_path, real_time_sequence):
-    print(real_time_sequence[-1].note)
-    print(existing_sequence[len(real_time_sequence)-1].note)
-    
     global count, start_time
-
-    # m = SequenceMatcher(None, real_time_sequence[-1], existing_sequence[len(real_time_sequence)-1])
 
     if count == 0:
         start_time = time.time()
@@ -47,26 +34,22 @@ def compare_midi_sequences(midi_file_path, real_time_sequence):
     else:
         end_time = time.time()
         diff = end_time - start_time
-    print(diff)
-    print(real_time[len(real_time_sequence) - 1]) 
 
     if real_time_sequence[-1].note == existing_sequence[len(real_time_sequence)-1].note:
         if abs(diff - real_time[len(real_time_sequence) - 1]) > 0.5:
-            print("YOU RLLY SUCK DUDE")
-        print("good job")
+            print("Correct note, but wrong timing!")
+        print("Great Job!")
     else:
-        print("you suck bitch")
+        print("Wrong Note!")
 
 
 # Real-time MIDI input
-with mido.open_input('MIDIIN2 (V25) 1') as port:
+PORT_NAME = "MIDIIN2 (V25) 1"
+with mido.open_input(PORT_NAME) as port:
     real_time_sequence = []
     for msg in port:
-        print(msg)
-
         if msg.type == "note_on":
             real_time_sequence.append(msg)
             real_time_sequence = list(real_time_sequence)
-
             midi_file_path = 'test.mid'
             compare_midi_sequences(midi_file_path, real_time_sequence)
