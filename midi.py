329d05f04623671cb33a7799d180bba985249298
Aman Choudhary <amanch@ic-tsloan-m081.local>
2023-12-06 11:23:15 -0500

    added score and rewind

--- midi.py
+++ midi.py
@@ -1,7 +1,7 @@
 import mido
 import time
 
-print("Welcome!")
+print("Welcome to KeyPerfection!")
 
 existing_sequence = []
 midi_file = mido.MidiFile('test.mid')
@@ -15,6 +15,8 @@ start_time = -1
 time_tracker = 0
 real_time = []
 
+score = 0
+
 for i, track in enumerate(midi_file.tracks):
     for msg in track:
         if type(msg) is not mido.MetaMessage:
@@ -25,7 +27,7 @@ for i, track in enumerate(midi_file.tracks):
 
 # Function to compare two MIDI sequences
 def compare_midi_sequences(midi_file_path, real_time_sequence):
-    global count, start_time
+    global count, start_time, score
 
     if count == 0:
         start_time = time.time()
@@ -33,23 +35,37 @@ def compare_midi_sequences(midi_file_path, real_time_sequence):
         diff = 0.0
     else:
         end_time = time.time()
-        diff = end_time - start_time
-
+        diff = end_time - start_time     
     if real_time_sequence[-1].note == existing_sequence[len(real_time_sequence)-1].note:
-        if abs(diff - real_time[len(real_time_sequence) - 1]) > 0.5:
-            print("Correct note, but wrong timing!")
-        print("Great Job!")
+        if abs(diff - real_time[len(real_time_sequence) - 1]) > 0.75:
+            print("wrong rhythm")
+            return False
+        else:
+            print("correct!")
+            score += 1
+            return True
     else:
-        print("Wrong Note!")
+        print("wrong note")
+        return False
 
 
 # Real-time MIDI input
-PORT_NAME = "MIDIIN2 (V25) 1"
-with mido.open_input(PORT_NAME) as port:
-    real_time_sequence = []
-    for msg in port:
-        if msg.type == "note_on":
-            real_time_sequence.append(msg)
-            real_time_sequence = list(real_time_sequence)
-            midi_file_path = 'test.mid'
-            compare_midi_sequences(midi_file_path, real_time_sequence)
+PORT_NAME_WINDOW = "MIDIIN2 (V25) 1"
+PORT_NAME_MAC = "V25 Out"
+
+with mido.open_input(PORT_NAME_MAC) as port:
+    done = False
+    while not done:
+        score = 0
+        real_time_sequence = []
+        count = 0
+        for msg in port:
+            if msg.type == "note_on":
+                real_time_sequence.append(msg)
+                real_time_sequence = list(real_time_sequence)
+                midi_file_path = 'test.mid'
+                done = compare_midi_sequences(midi_file_path, real_time_sequence)
+                if not done:
+                    break
+
+print("Good job! Your score is: ", score)
